7463b4bf924920fe8d7b8f813078fa591a28c533
Aurora Ziobrowski <aziobrow@gmail.com>
2018-02-12 09:39:39 -0700

    completed user story 1, adds top word to header

--- src/index.js
+++ src/index.js
@@ -1,5 +1,45 @@
 import $ from 'jquery'
 
+const url = 'https://wordwatch-api.herokuapp.com'
+
+const handleResponse = (response) => {
+  return response.json()
+  .then(json => {
+    if (!response.ok) {
+      const error = {
+        status: response.status,
+        statusText: response.statusText,
+      }
+      return Promise.reject(error);
+    }
+    return json
+  })
+}
+
+const getTopWord = () => {
+  fetch(`${url}/api/v1/top_word`)
+  .then((response) => handleResponse(response))
+  .then((wordInfo) => {
+    let word = getWord(wordInfo.word)
+    let count = getCount(wordInfo.word)
+    $('h3').append(`${word} (${count})`)
+  })
+  .catch((error) => console.error({ error }))
+}
+
+const getWord = (word) => {
+  for(var i in word){
+    return i
+    console.log(word[i])
+  }
+}
+
+const getCount = (word) => {
+  for(var i in word){
+    return word[i]
+  }
+}
+
 $(document).ready(() => {
-  // have fun!
+  getTopWord()
 })
